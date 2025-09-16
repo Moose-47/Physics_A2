32cb030428737163fe63c31f80a963ef7e4745ab
H00ver4ever <Tspivak4@gmail.com>
2025-09-15 20:25:22 -0400

    controls menu

---
+++ Assets/_Scripts/UI/ControlsMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+
+public class ControlsMenu : MonoBehaviour
+{
+
+    public Button continuebtn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        continuebtn.onClick.AddListener(() => SceneManager.LoadScene("Thomas"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
